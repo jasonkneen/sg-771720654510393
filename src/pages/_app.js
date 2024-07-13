fd1e50fe37be36474bbd3afdb63c9e3f8e3465f4
SoftGen <admin@softgen.ai>
2024-07-13 14:09:03 +0000

    SOFTGEN MAGIC

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,20 @@
 import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { AppProvider } from '@/context/AppContext';
+import { useEffect } from 'react';
+
+function clearLocalStorage() {
+  if (typeof window !== 'undefined') {
+    localStorage.clear();
+    console.log('Local storage cleared');
+  }
+}
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    clearLocalStorage();
+  }, []);
+
   return (
     <AppProvider>
       <Component {...pageProps} />
